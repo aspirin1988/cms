a3b591545dbb7f13a6689348218ca079df4e85c1
Sergey Demidov <aspirin_1988@mail.ru>
2016-08-15 17:32:46 +0600

    /start

--- database/migrations/2016_08_15_100818_create_my_db.php
+++ database/migrations/2016_08_15_100818_create_my_db.php
@@ -12,7 +12,37 @@ class CreateMyDb extends Migration
      */
     public function up()
     {
-        //
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent')->default(0);
+            $table->string('name');
+            $table->string('thumbnail',512);
+            $table->text('description');
+            $table->string('type')->default('category');
+        });
+
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent')->default(0);
+            $table->string('title');
+            $table->string('thumbnail',512);
+            $table->text('content');
+            $table->text('description');
+            $table->text('custom_fields');
+            $table->string('type')->default('page');
+        });
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent')->default(0);
+            $table->string('title');
+            $table->string('thumbnail',512);
+            $table->text('content');
+            $table->text('description');
+            $table->text('custom_fields');
+            $table->string('type')->default('post');
+        });
     }
 
     /**
